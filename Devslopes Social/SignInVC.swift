aea2beb578808f65291f113e797794fbc9eb4acd
Georgie Kirby <georgiekirby@georgies-mbp.home>
2017-08-15 18:56:30 +0100

    Completed set-up of Firsbase

---
+++ Devslopes Social/SignInVC.swift
@@ -0,0 +1,22 @@
+//
+//  SignInVC.swift
+//  Devslopes Social
+//
+//  Created by Georgie Kirby on 09/08/2017.
+//  Copyright © 2017 Scott Radford. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'Devslopes Social' do
+pod ‘Firebase/Core’
+pod ’Firebase/Auth’
+pod ’Firebase/Database’
+pod ’Firebase/Storage’
+end
