699f14b1ccf14593e66209f9f5813dbaa063b77f
Georgie Kirby <georgiekirby@georgies-mbp.home>
2017-08-16 10:03:20 +0100

    sign in custom classes complete

---
+++ Devslopes Social/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Utilities.swift
+//  Devslopes Social
+//
+//  Created by Georgie Kirby on 16/08/2017.
+//  Copyright © 2017 Scott Radford. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GREY: CGFloat = 120.0 / 255.0
+
+
+
---
+++ Devslopes Social/FancyTextField.swift
@@ -0,0 +1,28 @@
+//
+//  FancyTextField.swift
+//  Devslopes Social
+//
+//  Created by Georgie Kirby on 16/08/2017.
+//  Copyright © 2017 Scott Radford. All rights reserved.
+//
+
+import UIKit
+
+class FancyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+    }
+
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+}
